89512bd44a9ff2bb7519e362561a51d5f7f0ae78
JunKi, Hong <dylanhong920509@gmail.com>
2022-01-11 21:47:42 +0900

    layout vector table

--- App/TestApp/source/boot/startup.s
+++ App/TestApp/source/boot/startup.s
@@ -5,6 +5,23 @@
     .global vector_end
 
     vector_start:
+        LDR PC, undef_handler_addr
+        LDR PC, svc_handler_addr
+        LDR PC, pftch_abt_handler_addr
+        LDR PC, data_abt_handler_addr
+        B   .
+        LDR PC, irq_handler_addr
+        LDR PC, fiq_handler_addr
+
+        undef_handler_addr: .word reset_handler
+        svc_handler_addr: .word svc_handler
+        pftch_abt_handler_addr: .word pftch_abt_handler
+        data_abt_handler_addr: .word data_abt_handler_handler
+        irq_handler_addr: .word irq_handler_handler
+        fiq_handler_addr: .word fiq_handler_handler
+    vector_end:
+
+    reset_handler:
         /*
             [ SYS_ID Register ]
 
@@ -16,6 +33,12 @@
          */
         LDR R0, =0x10000000 /* R0 = 0x10000000 */
         LDR R1, [R0]        /* R1 = *(uint32_t*)0x10000000 --> 0x1780500*/
-    vector_end:
-        .space 1024, 0
+    
+    svc_handler:
+    pftch_abt_handler:
+    data_abt_handler_handler:
+    irq_handler_handler:
+    fiq_handler_handler:
+    dummy_handler:
+        B   .
 .end
