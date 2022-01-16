f02d8897a0b0176c799b9735da133d4bebb2f180
JunKi, Hong <dylanhong920509@gmail.com>
2022-01-16 17:20:33 +0900

    add library project

---
+++ Lib/realview-pb-a8/.config/options.cmake
@@ -0,0 +1,21 @@
+#++++++++++++++++++++++++++++++++++++++++++
+#           Compile Options
+#++++++++++++++++++++++++++++++++++++++++++
+# Common
+list(APPEND FLAGS -mcpu=${ARM_CPU})
+list(APPEND FLAGS -march=${ARM_CPU_ARCH})
+list(APPEND FLAGS -mfloat-abi=${ARM_FPU_ABI})
+if(NOT ${ARM_FPU} STREQUAL "")
+list(APPEND FLAGS -mfpu=${ARM_FPU})
+endif()
+
+# ASM
+list(APPEND ASFLAGS -D__ASM)
+
+# C
+list(APPEND CFLAGS -m${ARM_INST_SET})
+list(APPEND CFLAGS -std=${ARM_C_STANDARD})
+
+# CXX
+list(APPEND CXXFLAGS -m${ARM_INST_SET})
+list(APPEND CXXFLAGS -std=${ARM_CXX_STANDARD})
